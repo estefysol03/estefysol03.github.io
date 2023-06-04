81763c7f7df7a11803c7656dea3e3d18a718a907
estefysol03 <estefysol03@gmail.com>
2023-06-04 17:07:32 -0300

    cambios listos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const btnCart = document.querySelector('.container-cart-icon');
+const btnCart = document.querySelector('.container-icon');
 const containerCartProducts = document.querySelector('.container-cart-products');
 
 
@@ -8,11 +8,20 @@ btnCart.addEventListener('click', () => {
    containerCartProducts.classList.toggle('hidden-cart');
 });
 
-  var myCarousel = document.querySelector('#carouselExample');
+  /*var myCarousel = document.querySelector('#carouselExample');
   var carousel = new bootstrap.Carousel(myCarousel, {
     interval: 5000, // Cambia las diapositivas cada 5 segundos
     wrap: true // Permite recorrer el carrousel en bucle
   });
+*/
 
-
-
+  $(document).ready(function() {
+    $('.carousel').slick();
+  });
+  window.addEventListener('load', function() {
+    var carruselItems = document.querySelectorAll('.carrusel-item');
+    var carruselWidth = carruselItems[0].offsetWidth;
+    var carrusel = document.querySelector('.carrusel');
+    carrusel.style.width = carruselWidth * carruselItems.length + 'px';
+  });
+  
